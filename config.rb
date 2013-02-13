8ba2cf27a786c9b1dea3165b2ff62a246feaae03
Jens Ahrengot Boddum <ahrengot@gmail.com>
2013-02-13 13:56:26 +0100

    Added Compass

---
+++ config.rb
@@ -0,0 +1,12 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "styles/css"
+sass_dir = "styles/scss"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compact
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
